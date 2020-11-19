dd4d65b2bd3d038437f7cc8350b428af185f6814
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-11-19 09:33:57 +0700

    mengulang project dari 0

--- README.md
+++
@@ -1,2 +0,0 @@
-# epwmOnlineShop
-Untuk menyimpan hasil projek kedua di matakuliah web programming. Toko Online EPWM
--- admin/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "db_onlineshop";
-
-    $con = mysqli_connect($host, $user, $pass, $db);
- ?>
