8752f6f650a4dcf1aaec4553dfcf7452f7a95466
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-11-20 16:00:04 +0700

    Masalah tampilan berhasil diperbaiki. Ternyata masalah cache pada browser chrome yang tersimpan.

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <link rel="stylesheet" href="style.css">
     <title>Administrator epwmOnlineShop</title>
 </head>
 <body>
