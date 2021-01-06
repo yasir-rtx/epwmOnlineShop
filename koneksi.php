47ed85b961355df20073ca6d0f2224b6dd036f6c
yasir-rtx <yelfvankarlenstain@gmail.com>
2021-01-06 12:25:57 +0700

    koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+    /*Hostname*/ $host = "localhost";
+    /*Username*/ $user = "root";
+    /*Password*/ $pass = "";
+    /*Database*/ $db   = "db_onlineshop";
+    
+    $conn = mysqli_connect($host, $user, $pass, $db);
+?>
